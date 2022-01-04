7167ca1f0b7643a34ab1617b3726edafa95f9ed3
Fernando Valler <fernandovaller@gmail.com>
2022-01-04 11:05:40 -0300

    add tags Formatting

--- src/Html.php
+++ src/Html.php
@@ -77,4 +77,136 @@ class Html
     {
         return '<h6' . Parse::attr($data) . '>' . $label . '</h6>';
     }
+
+    /**
+     * Gerar tag <p></p>
+     *
+     * @param string $label
+     * @param array $data Attributes in format [key=>value]
+     * @return string
+     */
+    public static function p($label, $data = [])
+    {
+        return '<p' . Parse::attr($data) . '>' . $label . '</p>';
+    }
+
+    /**
+     * Gerar tag <b></b>
+     *
+     * @param string $label
+     * @param array $data Attributes in format [key=>value]
+     * @return string
+     */
+    public static function b($label, $data = [])
+    {
+        return '<b' . Parse::attr($data) . '>' . $label . '</b>';
+    }
+
+    /**
+     * Gerar tag <strong></strong>
+     *
+     * @param string $label
+     * @param array $data Attributes in format [key=>value]
+     * @return string
+     */
+    public static function strong($label, $data = [])
+    {
+        return '<strong' . Parse::attr($data) . '>' . $label . '</strong>';
+    }
+
+    /**
+     * Gerar tag <i></i>
+     *
+     * @param string $label
+     * @param array $data Attributes in format [key=>value]
+     * @return string
+     */
+    public static function i($label, $data = [])
+    {
+        return '<i' . Parse::attr($data) . '>' . $label . '</i>';
+    }
+
+    /**
+     * Gerar tag <em></em>
+     *
+     * @param string $label
+     * @param array $data Attributes in format [key=>value]
+     * @return string
+     */
+    public static function em($label, $data = [])
+    {
+        return '<em' . Parse::attr($data) . '>' . $label . '</em>';
+    }
+
+    /**
+     * Gerar tag <mark></mark>
+     *
+     * @param string $label
+     * @param array $data Attributes in format [key=>value]
+     * @return string
+     */
+    public static function mark($label, $data = [])
+    {
+        return '<mark' . Parse::attr($data) . '>' . $label . '</mark>';
+    }
+
+    /**
+     * Gerar tag <small></small>
+     *
+     * @param string $label
+     * @param array $data Attributes in format [key=>value]
+     * @return string
+     */
+    public static function small($label, $data = [])
+    {
+        return '<small' . Parse::attr($data) . '>' . $label . '</small>';
+    }
+
+    /**
+     * Gerar tag <del></del>
+     *
+     * @param string $label
+     * @param array $data Attributes in format [key=>value]
+     * @return string
+     */
+    public static function del($label, $data = [])
+    {
+        return '<del' . Parse::attr($data) . '>' . $label . '</del>';
+    }
+
+    /**
+     * Gerar tag <ins></ins>
+     *
+     * @param string $label
+     * @param array $data Attributes in format [key=>value]
+     * @return string
+     */
+    public static function ins($label, $data = [])
+    {
+        return '<ins' . Parse::attr($data) . '>' . $label . '</ins>';
+    }
+
+    /**
+     * Gerar tag <sub></sub>
+     *
+     * @param string $label
+     * @param array $data Attributes in format [key=>value]
+     * @return string
+     */
+    public static function sub($label, $data = [])
+    {
+        return '<sub' . Parse::attr($data) . '>' . $label . '</sub>';
+    }
+
+    /**
+     * Gerar tag <sup></sup>
+     *
+     * @param string $label
+     * @param array $data Attributes in format [key=>value]
+     * @return string
+     */
+    public static function sup($label, $data = [])
+    {
+        return '<sup' . Parse::attr($data) . '>' . $label . '</sup>';
+    }
 }
